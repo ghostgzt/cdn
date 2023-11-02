d432a82db94570b5ba716ef4ae1097006342055d
Gentle Kwan <ghostgzt@gmail.com>
2023-11-02 14:40:36 +0800

    sync

---
+++ sited/make.cmd
@@ -0,0 +1,2 @@
+node ./make.js
+pause
---
+++ sited/make.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+
+function base64encode(str) {
+	return Buffer.from(str).toString('base64');
+}
+function base64decode(str) {
+	return Buffer.from(str, 'base64').toString();
+}
+fs.readdir('./', (err, files) => {
+    if (err) throw err;
+    const moduleFiles = files.filter(file => file.endsWith('.sited'));
+    console.log(moduleFiles);
+    var data=moduleFiles.map(function(item){
+        var href="sited://data?"+base64encode("https://fastly.jsdelivr.net/gh/ghostgzt/cdn@master/sited/"+item);
+        return '<a href="'+href+'">'+item+'<href>'
+    });
+    console.log(data);
+    fs.writeFileSync("./index.html",data.join("\n"));
+});
